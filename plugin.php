267894b0f1a315c58e387f3228151083e5e59651
githubsync <githubsync@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 17:40:03 +0000

    commit 7536233cbfabc5ac49d4aff166e322a70a226ed9 Author: Dion Hulse <dd32@dd32.id.au> Date:   Fri Jan 23 11:39:55 2015 -0600
        Add Plugin File
    
    
    git-svn-id: https://plugins.svn.wordpress.org/test-plugin-3/trunk@1074268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: TEST PLUGIN
+Plugin URI: http://example.com/
+Description: Used by millions, Test Plugins are an important part of the world
+Version: 1.0
+Author: Dion Hulse
+Author URI: http://dd32.id.au/
+License: GPLv2 or later
+Text Domain: test-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+add_action( 'init', function() { echo '<h1>HELLO WORLD</h1>'; } );
